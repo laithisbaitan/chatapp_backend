4f8a7b422339747e345bbb9dc4e4419c5a25e3f3
laithisbaitan <80163581+laithisbaitan@users.noreply.github.com>
2024-06-07 01:18:26 +0300

    updated prod.ex

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :chatapp, ChatappWeb.Endpoint,
 
 config :chatapp, ChatappWeb.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: {:system, "DATABASE_URL"},
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 # Configures Swoosh API Client
